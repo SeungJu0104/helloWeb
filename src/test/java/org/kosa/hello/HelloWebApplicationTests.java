596332f276f590bcabe2cdec6c0019359ced899d
DESKTOP-684DG1A\SeungJu <skatmdwn23@naver.com>
2025-06-12 12:11:53 +0900

    테스트 코드 작성

--- src/test/java/org/kosa/hello/HelloWebApplicationTests.java
+++ src/test/java/org/kosa/hello/HelloWebApplicationTests.java
@@ -1,13 +1,28 @@
 package org.kosa.hello;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HelloWebApplicationTests {
 
-	@Test
-	void contextLoads() {
+//	@Test
+//	void contextLoads() {
+//	}
+	
+	private int add(int a, int b) {
+		return a + b;
 	}
+	
+	@Test
+	void testAdd() {
+		int a = 10, b = 20;
+		int sum = add(a, b);
+		
+		assertEquals(30, sum); // 실 테스트 코드
+	}
+	
 
 }
